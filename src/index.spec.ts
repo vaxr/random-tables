37a594adda013344c36e3d7b992e0baed8697e9f
Viktor Ricci <viktor@ricci.codes>
2021-07-10 15:12:21 -0400

    Implement happy case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,48 @@
-import {Hello} from './index';
+import {TableBuilder} from './index';
 
-describe('Hello', () => {
-    it('works', () => {
-        expect(Hello('World')).toBe('Hello World');
-    });
+describe('TableBuilder', () => {
+    interface TestCase {
+        name: string
+        input: {
+            entry: any,
+            weight: number,
+        }[]
+        expectedByRoll: {
+            roll: number,
+            expected: any
+        }[]
+    }
+
+    [
+        {
+            name: 'happy',
+            input: [
+                {entry: 'a', weight: 10},
+                {entry: 'b', weight: 10},
+                {entry: 'c', weight: 80},
+            ],
+            expectedByRoll: [
+                {roll: 0, expected: 'c'},
+                {roll: 0.799, expected: 'c'},
+                {roll: 0.801, expected: 'a'},
+                {roll: 0.899, expected: 'a'},
+                {roll: 0.901, expected: 'b'},
+                {roll: 0.999, expected: 'b'},
+            ],
+        }
+    ].forEach((tc: TestCase) => {
+        describe('happy', () => {
+            const builder = new TableBuilder()
+            for (const row of tc.input) {
+                builder.addEntryWithWeight(row.entry, row.weight)
+            }
+            const table = builder.build()
+            for (const row of tc.expectedByRoll) {
+                it (`${row.roll} --> ${row.expected}`, () => {
+                    const got = table.getEntryForRoll(row.roll)
+                    expect(got).toBe(row.expected)
+                })
+            }
+        });
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-export const Hello = (name: string) => `Hello ${name}`;
+export class TableBuilder {
+    rows: Row[] = []
+    nextOrdinal = 1
+
+    addEntryWithWeight(entry: any, weight: number) {
+        this.rows.push(new Row(entry, weight, this.nextOrdinal++))
+    }
+
+    build(): Table {
+        const table = new Table()
+        const totalWeight = this.rows.reduce((total: number, row: Row) => total + row.weight, 0)
+        this.rows
+            .sort((a, b) => (b.weight - a.weight) || (a.ordinal - b.ordinal))
+            .map(row => new Row(row.entry, row.weight / totalWeight, row.ordinal))
+            .forEach(row => table.rows.push(row))
+        return table
+    }
+}
+
+class Row {
+    entry: any
+    weight: number
+    ordinal: number
+
+    constructor(entry: any, weight: number, ordinal: number) {
+        this.entry = entry
+        this.weight = weight
+        this.ordinal = ordinal
+    }
+}
+
+class Table {
+    rows: Row[] = []
+
+    getEntryForRoll(roll: number): any {
+        for (const row of this.rows) {
+            roll -= row.weight
+            if (roll < 0) {
+                return row.entry
+            }
+        }
+        // TODO throw exception
+    }
+}
