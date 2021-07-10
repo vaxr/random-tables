22a9c411da4d54bdf5e2b298878f5bd1f03075af
Viktor Ricci <viktor@ricci.codes>
2021-07-10 15:34:15 -0400

    Lint and format

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,68 +1,68 @@
-import {TableBuilder} from './index';
+import { TableBuilder } from './index';
 
 describe('TableBuilder', () => {
-    interface TestCase {
-        name: string
-        input: {
-            entry: any
-            weight: number
-        }[]
-        expectedByRoll: {
-            roll: number
-            expected: any
-        }[]
-        errorByRoll: {
-            roll: number
-            error: any
-        }[]
-    }
+  interface TestCase {
+    name: string;
+    input: {
+      entry: any;
+      weight: number;
+    }[];
+    expectedByRoll: {
+      roll: number;
+      expected: any;
+    }[];
+    errorByRoll: {
+      roll: number;
+      error: any;
+    }[];
+  }
 
-    [
+  [
+    {
+      name: 'default case',
+      input: [
+        { entry: 'a', weight: 10 },
+        { entry: 'b', weight: 10 },
+        { entry: 'c', weight: 80 },
+      ],
+      expectedByRoll: [
+        { roll: 0, expected: 'c' },
+        { roll: 0.799, expected: 'c' },
+        { roll: 0.801, expected: 'a' },
+        { roll: 0.899, expected: 'a' },
+        { roll: 0.901, expected: 'b' },
+        { roll: 0.999, expected: 'b' },
+      ],
+      errorByRoll: [
         {
-            name: 'default case',
-            input: [
-                {entry: 'a', weight: 10},
-                {entry: 'b', weight: 10},
-                {entry: 'c', weight: 80},
-            ],
-            expectedByRoll: [
-                {roll: 0, expected: 'c'},
-                {roll: 0.799, expected: 'c'},
-                {roll: 0.801, expected: 'a'},
-                {roll: 0.899, expected: 'a'},
-                {roll: 0.901, expected: 'b'},
-                {roll: 0.999, expected: 'b'},
-            ],
-            errorByRoll: [
-                {
-                    roll: -0.001,
-                    error: TypeError,
-                },
-                {
-                    roll: 1,
-                    error: TypeError,
-                },
-            ]
-        }
-    ].forEach((tc: TestCase) => {
-        describe(tc.name, () => {
-            const builder = new TableBuilder()
-            for (const row of tc.input) {
-                builder.addEntryWithWeight(row.entry, row.weight)
-            }
-            const table = builder.build()
-            for (const row of tc.expectedByRoll) {
-                it (`${row.roll} --> ${row.expected}`, () => {
-                    const got = table.getEntryForRoll(row.roll)
-                    expect(got).toBe(row.expected)
-                })
-            }
-            for (const row of tc.errorByRoll) {
-                it (`${row.roll} trows ${row.error}`, () => {
-                    const call = () => table.getEntryForRoll(row.roll)
-                    expect(call).toThrow(row.error)
-                })
-            }
+          roll: -0.001,
+          error: TypeError,
+        },
+        {
+          roll: 1,
+          error: TypeError,
+        },
+      ],
+    },
+  ].forEach((tc: TestCase) => {
+    describe(tc.name, () => {
+      const builder = new TableBuilder();
+      for (const row of tc.input) {
+        builder.addEntryWithWeight(row.entry, row.weight);
+      }
+      const table = builder.build();
+      for (const row of tc.expectedByRoll) {
+        it(`${row.roll} --> ${row.expected}`, () => {
+          const got = table.getEntryForRoll(row.roll);
+          expect(got).toBe(row.expected);
         });
-    })
-})
+      }
+      for (const row of tc.errorByRoll) {
+        it(`${row.roll} trows ${row.error}`, () => {
+          const call = () => table.getEntryForRoll(row.roll);
+          expect(call).toThrow(row.error);
+        });
+      }
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
 export class TableBuilder {
-    rows: Row[] = []
-    nextOrdinal = 1
+  rows: Row[] = [];
+  nextOrdinal = 1;
 
-    addEntryWithWeight(entry: any, weight: number) {
-        this.rows.push(new Row(entry, weight, this.nextOrdinal++))
-    }
+  addEntryWithWeight(entry: any, weight: number) {
+    this.rows.push(new Row(entry, weight, this.nextOrdinal++));
+  }
 
-    build(): Table {
-        const table = new Table()
-        const totalWeight = this.rows.reduce((total: number, row: Row) => total + row.weight, 0)
-        this.rows
-            .sort((a, b) => (b.weight - a.weight) || (a.ordinal - b.ordinal))
-            .map(row => new Row(row.entry, row.weight / totalWeight, row.ordinal))
-            .forEach(row => table.rows.push(row))
-        return table
-    }
+  build(): Table {
+    const table = new Table();
+    const totalWeight = this.rows.reduce((total: number, row: Row) => total + row.weight, 0);
+    this.rows
+      .sort((a, b) => b.weight - a.weight || a.ordinal - b.ordinal)
+      .map((row) => new Row(row.entry, row.weight / totalWeight, row.ordinal))
+      .forEach((row) => table.rows.push(row));
+    return table;
+  }
 }
 
+// tslint:disable-next-line:max-classes-per-file
 class Row {
-    entry: any
-    weight: number
-    ordinal: number
+  entry: any;
+  weight: number;
+  ordinal: number;
 
-    constructor(entry: any, weight: number, ordinal: number) {
-        this.entry = entry
-        this.weight = weight
-        this.ordinal = ordinal
-    }
+  constructor(entry: any, weight: number, ordinal: number) {
+    this.entry = entry;
+    this.weight = weight;
+    this.ordinal = ordinal;
+  }
 }
 
+// tslint:disable-next-line:max-classes-per-file
 class Table {
-    rows: Row[] = []
+  rows: Row[] = [];
 
-    getEntryForRoll(roll: number): any {
-        if (roll < 0 || roll >= 1) {
-            throw new TypeError(`Roll must be in [0, 1), was: ${roll}`)
-        }
-        for (const row of this.rows) {
-            roll -= row.weight
-            if (roll < 0) {
-                return row.entry
-            }
-        }
+  getEntryForRoll(roll: number): any {
+    if (roll < 0 || roll >= 1) {
+      throw new TypeError(`Roll must be in [0, 1), was: ${roll}`);
     }
+    for (const row of this.rows) {
+      roll -= row.weight;
+      if (roll < 0) {
+        return row.entry;
+      }
+    }
+  }
 }
